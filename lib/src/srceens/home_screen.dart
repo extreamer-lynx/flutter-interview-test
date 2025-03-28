224a71572b8ef676924e13ee7d13d7bf5887b2f1
Vlad Franchuk <franchuk02@gmail.com>
2025-03-28 11:16:00 +0200

    Try to change with one integer

--- lib/src/srceens/home_screen.dart
+++ lib/src/srceens/home_screen.dart
@@ -14,16 +14,39 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _maxIntColorValue = 4294967295;
+  final int _maxColorValueForRGB = 0xff;
+  final int _redShift = 16;
+  final int _greenShift = 8;
+  final int _blueShift = 0;
+  final int _maxColorValue = 255;
   final _random = Random();
-  final _maxColorValue = 256;
+
   Color _color = Colors.white;
 
+  /// Changes the color of the screen to a random color.
+  /// Realization with calls of three _randoms.
   void _changeColor() {
     setState(() {
       _color = Color.fromRGBO(
         _random.nextInt(_maxColorValue),
         _random.nextInt(_maxColorValue),
         _random.nextInt(_maxColorValue),
+        1,
+      );
+    });
+  }
+
+  /// Changes the color of the screen to a random color.
+  /// Realization with calls of one _random.
+  @Deprecated("Challange to take one int and convert it to RGB")
+  void changeColorWithOneInt() {
+    final int intColor = _random.nextInt(_maxIntColorValue);
+    setState(() {
+      _color = Color.fromRGBO(
+        (intColor >> _redShift) & _maxColorValueForRGB,
+        (intColor >> _greenShift) & _maxColorValueForRGB,
+        (intColor >> _blueShift) & _maxColorValueForRGB,
         1,
       );
     });
